12e138616cf91f7f3e7b89e2ddfe329b246b4119
class5thsept <nunesleo@grinnell.edu>
2024-10-01 09:27:02 -0500

    Add Tally class and corresponding experiments

---
+++ src/main/java/edu/grinnell/csc207/util/Tally.java
@@ -0,0 +1,22 @@
+package edu.grinnell.csc207.util;
+/**
+ * Objects that Tally values.
+ */
+public class Tally extends BasicCounter {
+  /**
+   * Create a new Tally.
+   *
+   * @param start
+   *   The initial value of the tally.
+   */
+  /**
+   * Create a new Tally.
+   */
+  public Tally() {
+    super(0);
+  } // Tally()
+
+} // class Tally
+
+  
+
